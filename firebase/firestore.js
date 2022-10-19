bcbf83e285b85f0eddb5b5f98dd62b92ddbb5d49
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-10-19 10:19:55 -0500

    Se ha podido mostrar datos de la bd, ahora falta agregarle parametros al bot para poder hacerlo de manera personalizada

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,14 +1,28 @@
-const app = require('../keys/apikeyfirebase');
+//App solo la usamos para efecto de pruebas :)
+//const app = require('../keys/apikeyfirebase');
+const { collection, getDocs,  } = require('firebase/firestore/lite');
 
-const { collection, getDocs } = require('firebase/firestore/lite');
 
+//Buscar por medio de genero y autor
 const bookList = async(db) => {
-    const booksCol = collection(db, 'biblioteca/Matematicas/Lehmann');
+    const booksCol = collection(db, `biblioteca/Matematicas/Lehmann`);
     const bookSnapshot = await getDocs(booksCol);
     const bookList = bookSnapshot.docs.map(doc => doc.data());
     return bookList;
 }
+//Enlistar generos
+const generoList = async(db) => {
+    const querySnapshot = await getDocs(collection(db, "biblioteca"));
+    const genList = querySnapshot.docs.map(doc => doc.id)
+    return genList
+}
+//Enlistar autores
+
+//Enlistar titulos
+
+
 
 module.exports = {
-    bookList
+    bookList, generoList
 }
+
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-//const telebot = require('telebot');
-//const CONSTANTS = require('./keys/constants');
+const telebot = require('telebot');
+const CONSTANTS = require('./keys/constants');
 const app = require('./keys/apikeyfirebase');
-const db = require('./keys/apikeyfirebase');
-const { bookList } = require('./firebase/firestore');
-/*
+const { bookList, generoList } = require('./firebase/firestore');
+
 const bot = new telebot({
     token: CONSTANTS.telegram_token, // Required. Telegram Bot API token.
 });
@@ -16,20 +15,28 @@ bot.on(["/help"], (msg) => {
     bot.sendMessage(msg.from.id, 
     `/start: Inicia el bot. \n
      /help: Muestra los comandos. \n
-     /Buscar: Busca un libro. \n
+     /buscar: Busca un libro por autor y genero. \n
      /lista: Lista los libros. \n
      /generos: Lista los generos.`);
 });
 
+bot.on(["/buscar"], (msg) =>{
+    bookList(app.db).then((books) =>{
+        books.forEach(book =>{
+            bot.sendMessage(msg.from.id,
+                `Nombre: ${book.titulo}\nAutor: ${book.autor}\nEnlace: ${book.url}\n`            
+            )
+        })
+    })
+})
+
+bot.on(["/generos"], (msg)=>{
+    generoList(app.db).then((generos) =>{
+        generos.forEach(genero =>{
+            bot.sendMessage(msg.from.id, `${genero}` )
+        })
+    })
+})
+
 bot.start();
-*/
 
-
-// Aqui obtengo la lista de libros 
-bookList(db.db).then((books) => {   
-    books.forEach(book => {
-        console.log(book.titulo);
-        console.log(book.autor);
-        console.log(book.url);
-    });
-});
